7a184b8f87c90e9824f8909d383c69a9d78652ce
kokoro <cocosyu.11261126@gmail.com>
2022-08-17 02:49:22 +0900

    Add: ジオメトリを追加

--- three.js_scroll_website/src/main.ts
+++ three.js_scroll_website/src/main.ts
@@ -1,6 +1,5 @@
 import './style.css';
 import * as THREE from 'three';
-import { RepeatWrapping } from 'three';
 
 // canvas
 const canvas: any = document.querySelector('#webgl');
@@ -23,6 +22,20 @@ const renderer = new THREE.WebGL1Renderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(devicePixelRatio);
+
+// ジオメトリ
+const boxGeometry = new THREE.BoxGeometry(5, 5, 5, 10);
+const boxMaterial = new THREE.MeshNormalMaterial();
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(0, 0.5, -15);
+box.rotation.set(1, 1, 0);
+
+const torusGeometry = new THREE.TorusGeometry(8, 2, 16, 100);
+const torusMaterial = new THREE.MeshNormalMaterial();
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+torus.position.set(0, 0.5, 10);
+
+scene.add(box, torus);
 
 // アニメーション
 const animation = () => {
