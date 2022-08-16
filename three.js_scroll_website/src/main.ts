5bbf88b1d3130dd17d08b35390348054a4bc1564
kokoro <cocosyu.11261126@gmail.com>
2022-08-17 04:55:24 +0900

    Add: スクロールアニメーションを追加

--- three.js_scroll_website/src/main.ts
+++ three.js_scroll_website/src/main.ts
@@ -37,12 +37,80 @@ torus.position.set(0, 0.5, 10);
 
 scene.add(box, torus);
 
-// アニメーション
-const animation = () => {
-  requestAnimationFrame(animation);
-  renderer.render(scene, camera);
+let scrollAmount = 0;
+// スクロール量の計算
+document.body.onscroll = () => {
+  scrollAmount =
+    (document.documentElement.scrollTop /
+      (document.documentElement.scrollHeight - document.documentElement.clientHeight)) *
+    100;
+  console.log(scrollAmount);
 };
 
+// 線形補完
+function loop(x, y, a) {
+  return (1 - a) * x + a * y;
+}
+
+function scaleAmount(start, end) {
+  return (scrollAmount - start) / (end - start);
+}
+
+// スクロールアニメーション
+const animationScript = [];
+animationScript.push({
+  start: 0,
+  end: 40,
+  function() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    box.position.z = loop(-15, 2, scaleAmount(0, 40));
+    torus.position.z = loop(10, -20, scaleAmount(0, 40));
+  },
+});
+
+animationScript.push({
+  start: 40,
+  end: 60,
+  function() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    box.rotation.z = loop(1, Math.PI, scaleAmount(40, 60));
+  },
+});
+
+animationScript.push({
+  start: 60,
+  end: 80,
+  function() {
+    camera.lookAt(box.position);
+    camera.position.x = loop(0, 15, scaleAmount(60, 80));
+    camera.position.y = loop(1, 15, scaleAmount(60, 80));
+    camera.position.z = loop(10, 25, scaleAmount(60, 80));
+  },
+});
+
+animationScript.push({
+  start: 80,
+  end: 100,
+  function() {
+    camera.lookAt(box.position);
+  },
+});
+
+// スクロールアニメーションの実行
+function playScrollAnimation() {
+  animationScript.forEach((animation) => {
+    if (scrollAmount >= animation.start && scrollAmount < animation.end) animation.function();
+  });
+}
+const animation = () => {
+  requestAnimationFrame(animation);
+  playScrollAnimation();
+  renderer.render(scene, camera);
+  box.rotation.x += 0.02;
+  box.rotation.y += 0.02;
+};
 animation();
 
 // リサイズ処理
